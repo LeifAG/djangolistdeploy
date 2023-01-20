14a2ba499512625df84cfe44c41484d82be6c9bc
LeifAG <leif.hammar@gymnasium.ax>
2023-01-20 09:31:31 +0200

    allowed hosts again

--- listsite/settings.py
+++ listsite/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-)9!%@7ow_-1wd-&0$8-on$^!j!p@)ew=0ahujtlht5gu01z_xz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['web-production-d000.up.railway.app','127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://web-production-d000.up.railway.app']
+ALLOWED_HOSTS = ['web-production-2043.up.railway.app','127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-2043.up.railway.app']
 
 
 
